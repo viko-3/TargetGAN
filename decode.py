17d87675c61baacf95035174ed938f9533f3296a
cyy <55910336+Cyy1216@users.noreply.github.com>
2022-08-06 21:27:41 +0900

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,55 @@
+from rdkit import Chem
+import json
+import argparse
+import os, sys
+import numpy as np
+from autoencoder import autoencoder
+
+
+def decode(latent_mols_file, output_smiles_file_path=None, message='', model=None):
+    print("BEGIN")
+    print("Message: %s " % message)
+    sys.stdout.flush()
+    model = autoencoder.load_model(model)
+
+    if output_smiles_file_path is None:
+        output_smiles_file_path = os.path.join(os.path.dirname(latent_mols_file), 'decoded_smiles.smi')
+
+    with open(latent_mols_file, 'r') as f:
+        latent = json.load(f)
+
+    invalids = 0
+    batch_size = 256  # decoding batch size
+    n = len(latent)
+
+    with open(output_smiles_file_path, 'w') as smiles_file:
+        for indx in range(0, n // batch_size):
+            lat = np.array(latent[(indx) * 256:(indx + 1) * 256])
+            if indx % 10 == 0:
+                print("Batch [%d/%d] decoded, [Invalids: %s]" % (indx, n // batch_size + 1, invalids))
+                sys.stdout.flush()
+                smiles_file.flush()
+            # smiles, _ = model.predict_batch(lat, temp=0)
+            res = model.predict_batch(lat, temp=0)
+            if res is not None:
+                smiles, _ = res
+            else:
+                continue
+            for smi in smiles:
+                smiles_file.write(smi + '\n')
+                mol = Chem.MolFromSmiles(smi)
+                if not mol:
+                    invalids += 1
+    print("Decoding completed.")
+    print("Total: [%d] Fraction Valid: [0.%d]" % (n, (n - invalids) / n * 100))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Load and train a model")
+
+    parser.add_argument("--latent_mols_file", "-l", help="The path to a data file.", type=str, required=True)
+    parser.add_argument("--output_smiles_file_path", "-o", help="Prefix to the folder to save output smiles.", type=str)
+    parser.add_argument("--message", "-m", help="Message printed before training.", type=str)
+
+    args = {k: v for k, v in vars(parser.parse_args()).items() if v is not None}
+    decode(**args)
---
+++ encode.py
@@ -0,0 +1,46 @@
+from rdkit import Chem
+import json
+import argparse
+from autoencoder import autoencoder
+import os
+
+
+def encode(smiles_file, output_latent_file_path=None, encoder=None):
+    """
+    Encoding smile strings into latent vectors that are used to train GAN
+    :param smiles_file:
+    :param output_latent_file_path:
+    :return:
+    """
+    model = autoencoder.load_model(model_version=encoder)
+
+    # Input SMILES
+    smiles_in = []
+    with open(smiles_file, "r") as file:
+        line = file.readline()
+        while line:
+            smiles_in.append(line.strip('\n'))
+            line = file.readline()
+
+    # MUST convert SMILES to binary mols for the model to accept them (it re-converts them to SMILES internally)
+    mols_in = [Chem.rdchem.Mol.ToBinary(Chem.MolFromSmiles(smiles)) for smiles in smiles_in]
+    latent = model.transform(model.vectorize(mols_in))
+
+    # Writing JSON data
+    os.makedirs(os.path.dirname(output_latent_file_path), exist_ok=True)
+    with open(output_latent_file_path, 'w') as f:
+        json.dump(latent.tolist(), f)
+
+    print('Encoding completed!')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Load and train a model")
+
+    parser.add_argument("--smiles-file", "-sf", help="The path to a data file.", type=str, required=True)
+    parser.add_argument("--output_latent_file_path", "-o", help="Path to output smiles.", type=str)
+    parser.add_argument("--encoder",
+                        help="The data set the pre-trained heteroencoder has been trained on [chembl|moses] DEFAULT:chembl",
+                        type=str)
+    args = {k: v for k, v in vars(parser.parse_args()).items() if v is not None}
+    encode(**args)
