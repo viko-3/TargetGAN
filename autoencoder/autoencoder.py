a1073f56b09bce98c27129a16e1f054981438e8f
cyy <55910336+Cyy1216@users.noreply.github.com>
2022-08-14 14:09:42 +0900

    Add files via upload

---
+++ autoencoder/autoencoder.py
@@ -0,0 +1,23 @@
+from ddc_pub import ddc_v3 as ddc
+import os
+
+DEFAULT_MODEL_VERSION = 'chembl_pretrained'
+
+
+def load_model(model_version=None):
+    # Import model
+    if model_version == 'chembl':
+        model_name = 'chembl_pretrained'
+    elif model_version == 'moses':
+        model_name = 'moses_pretrained'
+    else:
+        model_name = DEFAULT_MODEL_VERSION
+
+
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), model_name)
+    print('Loading heteroencoder model from:', path)
+    model = ddc.DDC(model_name=path)
+
+    return model
+
+model = load_model()
